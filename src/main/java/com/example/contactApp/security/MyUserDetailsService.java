5ccae33ad49199d88a932c9acc4442410442d66d
marwa jafail <marwa.a.jafail@gmail.com>
2024-11-13 22:55:49 +0300

    add MyUserDetailsService

---
+++ src/main/java/com/example/contactApp/security/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.contactApp.security;
+import com.example.contactApp.model.User;
+import com.example.contactApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user.orElseThrow(() -> new UsernameNotFoundException("Username/email address not found")));
+    }
+}
